71623807d379469ff76bce7d9aea45573d2a4759
cable <cable870526@gmail.com>
2024-05-09 18:06:31 +0800

    更改

--- db.php
+++ db.php
@@ -1,50 +1,86 @@
-<?php 
-date_default_timezone_set("Asia/Taipei");
+<?php
+error_reporting(0);
+/* Session值定義 */
 session_start();
-class DB{
 
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=";
+// 資料庫設定
+define("db_server", "localhost");
+define("db_user", "root");
+define("db_password", "");
+define("db_name", "sttps");
+
+// 創建資料庫連接
+function createDatabaseConnection()
+{
+    $db = mysqli_connect(db_server, db_user, db_password, db_name);
+    if (!$db) {
+        die("無法對資料庫連線: " . mysqli_connect_error());
+    }
+    mysqli_set_charset($db, "utf8");
+    return $db;
+}
+
+// 創建PDO連接
+function createPDOConnection()
+{
+    $dsn = "mysql:host=" . db_server . ";dbname=" . db_name . ";charset=utf8";
+    try {
+        $pdo = new PDO($dsn, db_user, db_password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("無法對資料庫連線: " . $e->getMessage());
+    }
+    return $pdo;
+}
+
+// DB類
+class DB
+{
     protected $pdo;
     protected $table;
 
     public function __construct($table)
     {
-        $this->table=$table;
-        $this->pdo=new PDO($this->dsn,'root','');
+        $this->table = $table;
+        $this->pdo = createPDOConnection();
     }
 
-
-    function all( $where = '', $other = '')
+    function all($where = '', $other = '')
     {
         $sql = "select * from `$this->table` ";
-        $sql =$this->sql_all($sql,$where,$other);
+        $sql = $this->sql_all($sql, $where, $other);
         return  $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    function count( $where = '', $other = ''){
+    function count($where = '', $other = '')
+    {
         $sql = "select count(*) from `$this->table` ";
-        $sql=$this->sql_all($sql,$where,$other);
-        return  $this->pdo->query($sql)->fetchColumn(); 
+        $sql = $this->sql_all($sql, $where, $other);
+        return  $this->pdo->query($sql)->fetchColumn();
     }
-    private function math($math,$col,$array='',$other=''){
-        $sql="select $math(`$col`)  from `$this->table` ";
-        $sql=$this->sql_all($sql,$array,$other);
+    private function math($math, $col, $array = '', $other = '')
+    {
+        $sql = "select $math(`$col`)  from `$this->table` ";
+        $sql = $this->sql_all($sql, $array, $other);
         return $this->pdo->query($sql)->fetchColumn();
     }
-    function sum($col='', $where = '', $other = ''){
-        return  $this->math('sum',$col,$where,$other);
+    function sum($col = '', $where = '', $other = '')
+    {
+        return  $this->math('sum', $col, $where, $other);
     }
-    function max($col, $where = '', $other = ''){
-        return  $this->math('max',$col,$where,$other);
-    }  
-    function min($col, $where = '', $other = ''){
-        return  $this->math('min',$col,$where,$other);
-    }  
-    
+    function max($col, $where = '', $other = '')
+    {
+        return  $this->math('max', $col, $where, $other);
+    }
+    function min($col, $where = '', $other = '')
+    {
+        return  $this->math('min', $col, $where, $other);
+    }
+
     function find($id)
     {
         $sql = "select * from `$this->table` ";
-    
+
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= " where " . join(" && ", $tmp);
@@ -57,24 +93,25 @@ class DB{
         $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
         return $row;
     }
-    
-    function save($array){
-        if(isset($array['id'])){
+
+    function save($array)
+    {
+        if (isset($array['id'])) {
             $sql = "update `$this->table` set ";
-    
+
             if (!empty($array)) {
                 $tmp = $this->a2s($array);
             } else {
                 echo "錯誤:缺少要編輯的欄位陣列";
             }
-        
+
             $sql .= join(",", $tmp);
             $sql .= " where `id`='{$array['id']}'";
-        }else{
+        } else {
             $sql = "insert into `$this->table` ";
             $cols = "(`" . join("`,`", array_keys($array)) . "`)";
             $vals = "('" . join("','", $array) . "')";
-        
+
             $sql = $sql . $cols . " values " . $vals;
         }
 
@@ -84,7 +121,7 @@ class DB{
     function del($id)
     {
         $sql = "delete from `$this->table` where ";
-    
+
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= join(" && ", $tmp);
@@ -94,31 +131,33 @@ class DB{
             echo "錯誤:參數的資料型態比須是數字或陣列";
         }
         //echo $sql;
-    
+
         return $this->pdo->exec($sql);
     }
-    
+
     /**
      * 可輸入各式SQL語法字串並直接執行
      */
-    function q($sql){
+    function q($sql)
+    {
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-
     }
 
-    private function a2s($array){
+    private function a2s($array)
+    {
         foreach ($array as $col => $value) {
             $tmp[] = "`$col`='$value'";
         }
         return $tmp;
     }
 
-    private function sql_all($sql,$array,$other){
+    private function sql_all($sql, $array, $other)
+    {
 
         if (isset($this->table) && !empty($this->table)) {
-    
+
             if (is_array($array)) {
-    
+
                 if (!empty($array)) {
                     $tmp = $this->a2s($array);
                     $sql .= " where " . join(" && ", $tmp);
@@ -126,7 +165,7 @@ class DB{
             } else {
                 $sql .= " $array";
             }
-    
+
             $sql .= $other;
             // echo 'all=>'.$sql;
             // $rows = $this->pdo->query($sql)->fetchColumn();
@@ -135,7 +174,6 @@ class DB{
             echo "錯誤:沒有指定的資料表名稱";
         }
     }
-
 }
 
 function dd($array)
@@ -144,11 +182,7 @@ function dd($array)
     print_r($array);
     echo "</pre>";
 }
-
-function to($url){
+function to($url)
+{
     header("location:$url");
 }
-
-
-$Title=new DB('titles');
-?>
